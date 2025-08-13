5cd16c1128dc51c72f3501bf582569eeee256aed
Volodymyr Ryzhov <v.ryzhov.work@gmail.com>
2025-08-13 19:21:09 +0200

    feat(styles): add SCSS structure and font setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "/src/styles/variables" as *; @use "/src/styles/mixins" as *;`,
+        },
+      },
+    },
+  },
+});
